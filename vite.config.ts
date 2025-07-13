e8565e1be3507009de088e27d3b14a4fd68a2db6
Cooper <coop@coopeeo.xyz>
2025-07-13 11:44:40 -0500

    Update this stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), ViteImageOptimizer()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
