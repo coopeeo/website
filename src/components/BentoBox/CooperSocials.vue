4d224da75cd2912e9e50902331fc75ebdc8a5db2
Cooper <coop@coopeeo.xyz>
2025-07-26 18:29:16 -0500

    start on socials thingy

--- src/components/BentoBox/CooperSocials.vue
+++ src/components/BentoBox/CooperSocials.vue
@@ -2,16 +2,29 @@
 import { ref } from 'vue'
 import { Icon } from '@iconify/vue'
 
-const socials = ref([
-  { platform: 'Twitter', handle: '@coopeeo' },
-  { platform: 'Bluesky', handle: '@coopeeo.xyz' },
+interface Social {
+  platform: string
+  handle: string
+  icon: string
+  url?: string
+}
+
+const socials = ref(<Social[]>[
+  { platform: 'twitter', handle: '@coopeeo', icon: 'mditwitter' },
+  { platform: 'bluesky', handle: '@coopeeo.xyz', icon: 'fa7-brands:bluesky' },
 ])
+const socialIndex = ref(0)
 </script>
 
 <template>
-  <div class="socials-container">
-    <h3>Socials</h3>
-    <div class="socials">coopeeo</div>
+  <div class="social-container">
+    <div class="social-title">Socials</div>
+    <!-- prettier-ignore -->
+    <div class="social-handle" :data-platform="socials[socialIndex].platform" @pointerleave="() => socialIndex = socialIndex === (socials.length - 1) ? 0 : socialIndex + 1">
+      <div class="social-prefix">@</div>
+      coop<div class="underscore">_</div>eeo
+      <div class="social-icon"><Icon :icon="socials[socialIndex].icon" /></div>
+    </div>
     <div class="credits">
       Credit for the socials idea: <a target="_blank" href="https://pa.nley.contact">Panley</a>
     </div>
@@ -21,12 +34,43 @@ const socials = ref([
 <style lang="scss" scoped>
 @use '@catppuccin/palette/scss/mocha';
 
-.socials {
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
+.social-title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: mocha.$text;
+  margin-bottom: -1.75rem;
+}
+.social-prefix {
+  position: absolute;
+  display: inline;
+  z-index: -1;
+}
+.social-icon {
+  position: absolute;
+  right: 0.5rem;
+  top: 0.5rem;
+  color: mocha.$text;
+  z-index: -1;
+}
+.underscore {
+  display: inline;
+  position: absolute;
+  width: 0;
+  height: 0;
+  margin: 0;
+  padding: 0;
+  background-color: transparent;
+}
+.social-handle {
+  color: mocha.$text;
+  font-size: 7.875rem;
+  position: relative;
+  display: inline;
+  background-color: mocha.$base;
+  z-index: 2;
 }
 .credits {
   font-size: 0.875rem;
+  color: mocha.$subtext0;
 }
 </style>
