8eeeb77441abc034e80d1eb377fca0f7d800918e
Cooper <coop@coopeeo.xyz>
2025-07-26 19:31:01 -0500

    make cool social thing a reality holy crap this took so long lmao

--- src/components/BentoBox/CooperSocials.vue
+++ src/components/BentoBox/CooperSocials.vue
@@ -4,27 +4,45 @@ import { Icon } from '@iconify/vue'
 
 interface Social {
   platform: string
-  handle: string
+  suffix?: string
   icon: string
   url?: string
 }
 
 const socials = ref(<Social[]>[
-  { platform: 'twitter', handle: '@coopeeo', icon: 'mditwitter' },
-  { platform: 'bluesky', handle: '@coopeeo.xyz', icon: 'fa7-brands:bluesky' },
+  { platform: 'twitter', icon: 'mdi-twitter', url: 'https://twitter.com/@coop_eeo' },
+  {
+    platform: 'bluesky',
+    suffix: '.xyz',
+    icon: 'fa7-brands:bluesky',
+    url: 'https://bsky.app/profile/coopeeo.xyz',
+  },
+  { platform: 'github', icon: 'mdi-github', url: 'https://github.com/coopeeo' },
+  { platform: 'steam', icon: 'mdi-steam', url: 'https://steamcommunity.com/id/coopeeo' },
 ])
 const socialIndex = ref(0)
+let socialDebounce: boolean = false
+const nextSocial = () => {
+  if (socialDebounce) return
+  socialDebounce = true
+  setTimeout(() => {
+    socialIndex.value = (socialIndex.value + 1) % socials.value.length
+  }, 300)
+  setTimeout(() => {
+    socialDebounce = false
+  }, 400)
+}
 </script>
 
 <template>
   <div class="social-container">
     <div class="social-title">Socials</div>
     <!-- prettier-ignore -->
-    <div class="social-handle" :data-platform="socials[socialIndex].platform" @pointerleave="() => socialIndex = socialIndex === (socials.length - 1) ? 0 : socialIndex + 1">
+    <a :href="socials[socialIndex].url" class="social-handle" :data-platform="socials[socialIndex].platform" @pointerleave="nextSocial">
       <div class="social-prefix">@</div>
-      coop<div class="underscore">_</div>eeo
-      <div class="social-icon"><Icon :icon="socials[socialIndex].icon" /></div>
-    </div>
+      coop<div class="underscore">_</div><span class="ending2">eeo</span><span class="ending">eeo</span>
+      <div class="social-suffix">{{ socials[socialIndex].suffix }}<Icon :icon="socials[socialIndex].icon" /></div>
+    </a>
     <div class="credits">
       Credit for the socials idea: <a target="_blank" href="https://pa.nley.contact">Panley</a>
     </div>
@@ -45,12 +63,28 @@ const socialIndex = ref(0)
   display: inline;
   z-index: -1;
 }
-.social-icon {
+.social-suffix {
+  --y-offset: -27px;
   position: absolute;
   right: 0.5rem;
-  top: 0.5rem;
-  color: mocha.$text;
+  top: 1rem;
+  font-size: 7rem;
+  transform: translateY(var(--y-offset));
+  color: mocha.$text !important;
   z-index: -1;
+  transition:
+    transform 0.3s ease,
+    color 0.3s ease;
+  .social-handle:hover > & {
+    transform: translateX(120px) translateY(var(--y-offset));
+    color: mocha.$rosewater;
+  }
+  .social-handle[data-platform='bluesky']:hover > & {
+    transform: translateX(315px) translateY(var(--y-offset));
+  }
+  .social-handle[data-platform='twitter']:hover > & {
+    transform: translateX(calc(120px + 63px)) translateY(var(--y-offset));
+  }
 }
 .underscore {
   display: inline;
@@ -60,6 +94,32 @@ const socialIndex = ref(0)
   margin: 0;
   padding: 0;
   background-color: transparent;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  .social-handle[data-platform='twitter']:hover > & {
+    opacity: 1;
+  }
+}
+.ending2 {
+  opacity: 0;
+  visibility: hidden;
+}
+.ending {
+  --x-offset: 274px;
+  position: absolute;
+  background-color: transparent;
+  z-index: 1;
+  width: 0;
+  height: 0;
+  margin: 0;
+  padding: 0;
+  left: 0;
+  transform: translateX(var(--x-offset));
+  color: mocha.$text;
+  transition: transform 0.3s ease;
+  .social-handle[data-platform='twitter']:hover > & {
+    transform: translateX(calc(var(--x-offset) + 63px));
+  }
 }
 .social-handle {
   color: mocha.$text;
